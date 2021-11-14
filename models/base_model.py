cdf46da0a0c922a50195451e4873ae3f92108a70
rmatovu987 <rmatovu987@gmail.com>
2021-11-14 21:29:01 +0300

    ...

--- models/base_model.py
+++ models/base_model.py
@@ -9,24 +9,14 @@ import models
 
 class BaseModel:
     """Class for BaseModel"""
-
     def __init__(self, *args, **kwargs):
-        """BaseModel Instance Initializer
-
-         Args:
-            - *args: list of arguments
-            - **kwargs: dict of key-values arguments
-        """
-        if kwargs is not None and kwargs != {}:
-            for key in kwargs:
-                if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(
-                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                else:
-                    self.__dict__[key] = kwargs[key]
+        """BaseModel Instance Initializer"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key in ["created_at", "updated_at"]:
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key not in ["__class__"]:
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
